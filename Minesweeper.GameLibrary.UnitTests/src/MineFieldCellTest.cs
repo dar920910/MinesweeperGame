80a7ee4077db21de216cfb1a2175a4292be792fa
Denis Rezepov <me@dar920910.ru>
2023-11-09 13:59:10 +0300

    Implemented the MineFieldCell class.

--- Minesweeper.GameLibrary.UnitTests/src/MineFieldCellTest.cs
+++ Minesweeper.GameLibrary.UnitTests/src/MineFieldCellTest.cs
@@ -15,11 +15,222 @@ public class MineFieldCellTest
     private const byte TestFieldCellRow = 1;
     private const byte TestFieldCellCol = 1;
 
+    private const string NonActivatedFieldCellStatus = " ";
+    private const string ActivatedExplosiveFieldCellStatus = "X";
+    private const string NonActivatedExplosiveFieldCellStatus = "M";
+
     /// <summary>
     /// Инициализирует экземпляр тестового класса <see cref="MineFieldCellTest"/> по умолчанию.
     /// </summary>
     public MineFieldCellTest()
     {
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 0 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual0()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { false, false, false },
+            { false, false, false },
+            { false, false, false },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 0);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "0", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 1 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual1()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, false, false },
+            { false, false, false },
+            { false, false, false },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 1);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "1", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 2 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual2()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, true, false },
+            { false, false, false },
+            { false, false, false },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 2);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "2", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 3 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual3()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, true, true },
+            { false, false, false },
+            { false, false, false },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 3);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "3", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 4 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual4()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, true, true },
+            { false, false, true },
+            { false, false, false },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 4);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "4", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 5 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual5()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, true, true },
+            { false, false, true },
+            { false, false, true },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 5);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "5", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 6 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual6()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, true, true },
+            { false, false, true },
+            { false, true, true },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 6);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "6", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 7 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual7()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, true, true },
+            { false, false, true },
+            { true, true, true },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 7);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "7", actual: fieldCell.GameStatus);
+    }
+
+    /// <summary>
+    /// Тестирует получение визуального статуса активированной ячейки, вокруг которой расположено 8 заминированных ячеек.
+    /// </summary>
+    [Fact]
+    public void GetGameStatusForActivatedFieldCell_WhenNeighbourCellsWithMinesAroundAreEqual8()
+    {
+        MineField mineField = new (new bool[,]
+        {
+            { true, true, true },
+            { true, false, true },
+            { true, true, true },
+        });
+
+        MineFieldCell fieldCell = new (
+            position: new FieldCellPosition(1, 1),
+            isExplosiveCell: false,
+            countOfCellsWithMinesAround: 8);
+
+        fieldCell.Activate();
+
+        Assert.Equal(expected: "8", actual: fieldCell.GameStatus);
     }
 
     /// <summary>
@@ -31,7 +242,7 @@ public class MineFieldCellTest
         MineField mineField = CreateTestMineFieldByDefault();
         MineFieldCell fieldCell = CreateTestFieldCellByDefault(mineField);
 
-        Assert.Equal(expected: " ", actual: fieldCell.GameStatus);
+        Assert.Equal(expected: NonActivatedFieldCellStatus, actual: fieldCell.GameStatus);
     }
 
     private static MineField CreateTestMineFieldByDefault() => new (new bool[,]
--- Minesweeper.GameLibrary/src/MineFieldCell.cs
+++ Minesweeper.GameLibrary/src/MineFieldCell.cs
@@ -9,6 +9,17 @@ namespace Minesweeper.GameLibrary;
 /// </summary>
 public class MineFieldCell
 {
+    /// <summary>
+    /// Представляет константное значение для статуса заминированной ячейки, активированной пользователем.
+    /// </summary>
+    public const string ActivatedExplosiveFieldCellStatus = "X";
+
+    /// <summary>
+    /// Представляет константное значение для статуса неактивированной заминированной ячейки,
+    /// который присваивается данной ячейке в результате успешного прохождения игры пользователем.
+    /// </summary>
+    public const string NonActivatedExplosiveFieldCellStatus = "M";
+
     private const char NonActivatedFieldCellStatusSymbol = ' ';
 
     private readonly bool isExplosiveCell;
@@ -30,7 +41,6 @@ public class MineFieldCell
         this.isActivatedFieldCell = false;
 
         this.Position = position;
-        this.GameStatus = NonActivatedFieldCellStatusSymbol.ToString();
     }
 
     /// <summary>
@@ -41,5 +51,51 @@ public class MineFieldCell
     /// <summary>
     /// Представляет текущий игровой статус ячейки.
     /// </summary>
-    public string GameStatus { get; }
+    public string GameStatus
+    {
+        get
+        {
+            if (this.isActivatedFieldCell)
+            {
+                if (this.isExplosiveCell)
+                {
+                    return ActivatedExplosiveFieldCellStatus;
+                }
+                else
+                {
+                    switch (this.countOfCellsWithMinesAround)
+                    {
+                        case 0:
+                            return "0";
+                        case 1:
+                            return "1";
+                        case 2:
+                            return "2";
+                        case 3:
+                            return "3";
+                        case 4:
+                            return "4";
+                        case 5:
+                            return "5";
+                        case 6:
+                            return "6";
+                        case 7:
+                            return "7";
+                        case 8:
+                            return "8";
+                    }
+                }
+            }
+
+            return NonActivatedFieldCellStatusSymbol.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Активирует текущую ячейку минного поля.
+    /// </summary>
+    public void Activate()
+    {
+        this.isActivatedFieldCell = true;
+    }
 }
